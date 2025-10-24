faa59da6c5a73dbc5431965cb38d6c4a733a9ebf
Brian Holt <btholt@gmail.com>
2025-10-23 19:20:10 -0700

    Add speed insights and analytics

--- 09-with-tests/src/app/layout.tsx
+++ 09-with-tests/src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { StackProvider, StackTheme } from "@stackframe/stack";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { stackClientApp } from "../stack/client";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -34,6 +36,8 @@ export default function RootLayout({
           <StackTheme>
             <NavBar />
             {children}
+            <Analytics />
+            <SpeedInsights />
           </StackTheme>
         </StackProvider>
       </body>
